821e64c31b9d7e96c8307f3bd2dfa4bebedca2f3
dema99 <76456584+dema99@users.noreply.github.com>
2024-05-13 09:33:37 +0200

    Add files via upload

---
+++ src/main/java/org/example/Question.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class Question {
+    private Integer questionId;
+    private String text;
+    private String intent;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Integer categoryId;
+    private Category category;
+}
